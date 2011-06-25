cf50e109f1142ad62365ec335c402c1e64081844
punytan <punytan@gmail.com>
2011-06-25 14:36:17 +0900

    initial commit

---
+++ lib/App/cpanmigrate.pm
@@ -0,0 +1,32 @@
+package App::cpanmigrate;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::cpanmigrate -
+
+=head1 SYNOPSIS
+
+  use App::cpanmigrate;
+
+=head1 DESCRIPTION
+
+App::cpanmigrate is
+
+=head1 AUTHOR
+
+punytan E<lt>punytan@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
